94b886fe0403734e8c02a99e31c39e95f85eb146
DejanBabic <76682031+BabicDejan@users.noreply.github.com>
2025-11-08 01:03:51 +0100

    Delete react-app directory

--- react-app/src/components/handleButton.tsx
+++
@@ -1,8 +0,0 @@
-function Button(){
-    const handleClick = () => alert("Dugme je kliknuto!");
-    return (
-        <button style={{marginTop:"10px", backgroundColor:"red"}} 
-        onClick={handleClick}>Klikni me</button>
-    )
-}
-export default Button
